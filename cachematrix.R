a1817b6b82524b3033d690ad59d6b623bc9699fe
fritz-lowrey <fritz_lowrey@yahoo.com>
2015-02-22 15:59:25 -0500

    Programming Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,63 @@
-## Put comments here that give an overall description of what your
-## functions do
+## CacheMatrix.R - Programming assignment 2 for R programming.
 
-## Write a short comment describing this function
+## Cache a matrix or update the cached matrix if the new one differs from the
+## cached version
+makeCacheMatrix <- function(x = matrix()) {  
+  
+  set <- function(y = matrix())
+  {
+    cacheMatrix <<- y
+    cacheInverse <<- NULL
+  }  
 
-makeCacheMatrix <- function(x = matrix()) {
-
+  setInverse <- function(z = matrix())
+  {
+    cacheInverse <<- z
+  }
+  
+  get <- function() 
+  {
+    if(exists("cacheMatrix"))
+    {
+      return(cacheMatrix)
+    }
+    else
+    {
+      return(NULL)
+    }
+  }
+  
+  getInverse <- function() 
+  {
+    if(exists("cacheInverse"))
+    {
+      return(cacheInverse)
+    }
+    else
+    {
+      return(NULL)
+    }
+  }
+    
+  list(set = set, get = get, setInverse = setInverse, getInverse=getInverse)
 }
 
 
-## Write a short comment describing this function
-
+##This function computes the inverse of the special "matrix" returned by 
+## makeCacheMatrix above. If the inverse has already been calculated (and 
+## the matrix has not changed), then the cachesolve should retrieve the 
+## inverse from the cache.
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  cached <- makeCacheMatrix(NULL)
+  
+  thisMatrix <- cached$get()
+  
+  if(is.null(thisMatrix) | all(thisMatrix == x))
+    cached$set(x)
+  
+  if(is.null(cached$getInverse()))
+    cached$setInverse(solve(x))
+  
+  cached$getInverse()
 }
